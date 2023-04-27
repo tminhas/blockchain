dade3522305bd1435df8b3c42650f09295df0eb7
tminhas <86493097+tminhas@users.noreply.github.com>
2023-04-27 16:15:38 -0300

    Update client_plc3.py

--- Bolsa/clients/client_plc3.py
+++ Bolsa/clients/client_plc3.py
@@ -20,12 +20,12 @@ while True:
     # Read the values of the PLC inputs and outputs
     
     #input era pra ter 10 tags mas só tem 8
-    inp = opc.read('Separating.GV_S_Chegada_3','Separating.GV_S_Presenca_3','Separating.GV_S_Altura_3',
+    inp = opc.read(['Separating.GV_S_Chegada_3','Separating.GV_S_Presenca_3','Separating.GV_S_Altura_3',
                    'Separating.GV_S_Descarte_3','Separating.GV_S_Final_3','Separating.GV_B_Start_3',
-                   'Separating.GV_B_Stop_3','Separating.GV_B_Reset_3')
-    out = opc.read('Separating.GV_Motor_P_3', 'Separating.GV_Motor_D_3', 'Separating.GV_Pino_3',
+                   'Separating.GV_B_Stop_3','Separating.GV_B_Reset_3'])
+    out = opc.read(['Separating.GV_Motor_P_3', 'Separating.GV_Motor_D_3', 'Separating.GV_Pino_3',
                     'Separating.GV_Garra_3', 'Separating.GV_IP_N_FO_3', 'Separating.GV_LUZ_Start_3',
-                    'Separating.GV_LUZ_Reset_3', 'Separating.GV_LUZ_Q1_3','Separating.GV_LUZ_Q2_3')
+                    'Separating.GV_LUZ_Reset_3', 'Separating.GV_LUZ_Q1_3','Separating.GV_LUZ_Q2_3'])
     
     inputs = [int(inpu[0]) for inpu in inp]
     outputs = [int(output[0]) for output in out]
