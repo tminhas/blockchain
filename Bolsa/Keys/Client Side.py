0cb2424657d8b458e37409f8d981a8d26c8a5eea
tminhas <86493097+tminhas@users.noreply.github.com>
2023-07-18 21:28:45 -0300

    Update Client Side.py

--- Bolsa/Keys/Client Side.py
+++ Bolsa/Keys/Client Side.py
@@ -1,32 +1,32 @@
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
-# Generate a new RSA private key
-private_key = rsa.generate_private_key(
+# Generate a new RSA private key for the client
+client_private_key = rsa.generate_private_key(
     public_exponent=65537,
     key_size=2048,
 )
 
-# Serialize the private key to PEM format
-private_key_pem = private_key.private_bytes(
+# Serialize the client's private key to PEM format
+client_private_key_pem = client_private_key.private_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PrivateFormat.PKCS8,
     encryption_algorithm=serialization.NoEncryption()
 )
 
-# Save the private key to a file
+# Save the client's private key to a file
 with open('client_private_key.pem', 'wb') as key_file:
-    key_file.write(private_key_pem)
+    key_file.write(client_private_key_pem)
 
-# Extract the public key from the private key
-public_key = private_key.public_key()
+# Extract the client's public key from the private key
+client_public_key = client_private_key.public_key()
 
-# Serialize the public key to PEM format
-public_key_pem = public_key.public_bytes(
+# Serialize the client's public key to PEM format
+client_public_key_pem = client_public_key.public_bytes(
     encoding=serialization.Encoding.PEM,
     format=serialization.PublicFormat.SubjectPublicKeyInfo
 )
 
-# Save the public key to a file
+# Save the client's public key to a file
 with open('client_public_key.pem', 'wb') as key_file:
-    key_file.write(public_key_pem)
+    key_file.write(client_public_key_pem)
